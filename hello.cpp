77ee164e3b7be1b715ef0aa41c03075a0a8c3e6b
Augusto Coronel <aocoronel@proton.me>
2026-04-09 08:41:52 -0300

    cpp: defer and operator for '=' and '[]' madness

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,24 @@
+#include <cstdio>
 #include <stdio.h>
 
 struct MyClass {
     int myNum;
+    bool err;
     const char *myString;
     void myMethod() {
         printf("%s\n", "Hello World!");
         return;
+    }
+    void myMethod2() {
+        printf("%s\n", "Hello World!");
+        return;
+    }
+    template <typename T> T add(T x, T y) {
+        if (x > y) {
+            err = true;
+            return 0;
+        }
+        return x + y;
     }
 };
 
@@ -35,22 +48,64 @@ namespace vec {
 struct Vec2 {
     float x, y;
 
-    Vec2 operator+(const Vec2 &other) const {
+    Vec2 operator+(const Vec2 other) const {
         return { x + other.x, y + other.y };
     }
 
-    Vec2 operator-(const Vec2 &other) const {
+    Vec2 operator-(const Vec2 other) const {
         return { x - other.x, y - other.y };
     }
 
-    Vec2 operator/(const Vec2 &other) const {
+    Vec2 operator/(const Vec2 other) const {
         return { x / other.x, y / other.y };
     }
 
-    Vec2 operator*(const Vec2 &other) const {
+    Vec2 operator*(const Vec2 other) const {
         return { x * other.x, y * other.y };
     }
+
+    void operator=(const Vec2 other) {
+        x = other.x;
+        y = other.y;
+    }
+
+    float operator[](size_t idx) const {
+        return x + y + idx;
+    }
+
+    void print(void) {
+        printf("[%f, %f]\n", x, y);
+    }
 };
+} // namespace vec
+
+#define lambda auto
+
+template <typename F> struct scoped_defer {
+    F f;
+    explicit scoped_defer(F f)
+            : f(f) {
+    }
+    ~scoped_defer() {
+        f();
+    }
+};
+
+#define CAT(a, b) CAT_IMPL(a, b)
+#define CAT_IMPL(a, b) a##b
+#define defer(code) auto CAT(defer_t, __LINE__) = scoped_defer([&]() { code; })
+
+// Usage
+void f2() {
+    // FILE *fp = fopen("file.txt", "r");
+    // if (!fp) return;
+    // DEFER(fclose(fp)); // fclose runs when f() exits
+    defer({
+        printf("=i\n");
+        printf("=i\n");
+        printf("=i\n");
+    });
+    printf("hello\n");
 }
 
 int main() {
@@ -68,14 +123,21 @@ int main() {
 
     int z = 10;
 
-    auto f = [z](int x) { return x + z; };
+    lambda f = [z](int x) { return x + z; };
 
     printf("%d\n", f(20));
 
     vec::Vec2 a{ 1, 2 }, b{ 3, 4 };
     vec::Vec2 c = a / b;
-    printf("%f %f\n", c.x, c.y);
+    c = a;
+    c.print();
+    f2();
 
+    printf("%f\n", c[20]);
+
+    vec::Vec2 za{ 1, 2 };
+    vec::Vec2 *zw = &za;
+    printf("%f\n", zw->x);
     return 0;
 }
 // clang++ hello.cpp -fno-exceptions -fno-rtti -nostdlib++
